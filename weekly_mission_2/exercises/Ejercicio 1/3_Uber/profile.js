3082c3cd14db470a96edcd4c9a14bc549d997bfe
FranciscoGomezSKD <francisco.gomez.uam@hotmail.com>
2022-04-13 22:36:21 -0500

    update
    se sube ejercicio 1 - uber

---
+++ weekly_mission_2/exercises/Ejercicio 1/3_Uber/profile.js
@@ -0,0 +1,9 @@
+const profile = {
+    name: "frank",
+    phone: "5544556611",
+    location: "cdmx",
+    languaje: "Spanish",
+    mail: "contact@mail.com"
+}
+
+console.log(profile)
---
+++ weekly_mission_2/exercises/Ejercicio 1/3_Uber/travel.js
@@ -0,0 +1,8 @@
+const travel = {
+    date: "13/04/22",
+    cost: "$150",
+    payment: "paypal",
+    itinerary: ["casa", "trabajo"]
+}
+
+console.log(travel)
